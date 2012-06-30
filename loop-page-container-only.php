b6fcf9d154d97badb65bb9c7da63baee3d4c9577
JohnIsCoding <info@pigeondesign.net>
2012-06-30 10:30:29 +0100

    Removed bad idea

--- loop-page-container-only.php
+++ loop-page-container-only.php
@@ -2,19 +2,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php roots_post_before(); ?>
     <?php roots_post_inside_before(); ?>
-     
-     
-   
-      
-      
-      
-      
-      <?php 
-if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-  the_post_thumbnail();
-} 
-?>
-      
+       
       <?php the_content(); ?>
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
