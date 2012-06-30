495f309f554c5df0e787c9edc49972fb552aeb9d
JohnIsCoding <info@pigeondesign.net>
2012-06-30 09:25:32 +0100

    Added featured image to loop

--- loop-page-container-only.php
+++ loop-page-container-only.php
@@ -2,9 +2,24 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php roots_post_before(); ?>
     <?php roots_post_inside_before(); ?>
+     
+     
+    <!--
+ 
       <div class="page-header">
-      	<h1><?php the_title(); ?></h1>
+      	<h1><?php //the_title(); ?></h1>
       </div>
+      
+-->
+      
+      
+      
+      <?php 
+if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+  the_post_thumbnail();
+} 
+?>
+      
       <?php the_content(); ?>
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
     <?php roots_post_inside_after(); ?>
