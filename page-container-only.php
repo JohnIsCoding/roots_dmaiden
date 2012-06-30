5d5ae75890424af39c79bbcf0e3643587ccf3df7
JohnIsCoding <info@pigeondesign.net>
2012-06-30 09:02:20 +0100

    Added custom loop

--- page-container-only.php
+++ page-container-only.php
@@ -8,7 +8,7 @@ get_header(); ?>
     <?php roots_main_before(); ?>
       <div id="main" role="main">
         <?php roots_loop_before(); ?>
-        <?php get_template_part('loop', 'page'); ?>
+        <?php get_template_part('loop', 'page-container-only'); ?>
         <?php roots_loop_after(); ?>
       </div><!-- /#main -->
     <?php roots_main_after(); ?>
