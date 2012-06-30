7328c3c0274753595460edc86065307c7d8e64e0
JohnIsCoding <info@pigeondesign.net>
2012-06-30 09:25:58 +0100

    Removed page header

--- loop-page-container-only.php
+++ loop-page-container-only.php
@@ -4,13 +4,8 @@
     <?php roots_post_inside_before(); ?>
      
      
-    <!--
- 
-      <div class="page-header">
-      	<h1><?php //the_title(); ?></h1>
-      </div>
+   
       
--->
       
       
       
