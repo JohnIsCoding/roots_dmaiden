9d85c9ba86e03a297157dd3959406063b12ae44b
JohnIsCoding <info@pigeondesign.net>
2012-07-04 10:15:21 +0100

    Template for gallery

--- functions.php
+++ functions.php
@@ -41,3 +41,47 @@ function roots_setup() {
 }
 
 add_action('after_setup_theme', 'roots_setup');
+
+
+
+//register a new post type
+add_action( 'init', 'register_cpt_gallery' );
+    function register_cpt_gallery() {
+    $labels = array(
+    'name' => _x( 'Exhibits', 'gallery' ),
+    'singular_name' => _x( 'Exhibit', 'gallery' ),
+    'add_new' => _x( 'Add New', 'gallery' ),
+    'add_new_item' => _x( 'Add New Exhibit', 'gallery' ),
+    'edit_item' => _x( 'Edit Exhibit', 'gallery' ),
+    'new_item' => _x( 'New Exhibit', 'gallery' ),
+    'view_item' => _x( 'View Exhibit', 'gallery' ),
+    'search_items' => _x( 'Search the Gallery', 'gallery' ),
+    'not_found' => _x( 'Not found', 'gallery' ),
+    'not_found_in_trash' => _x( 'Not found in Trash', 'gallery' ),
+    'parent_item_colon' => _x( 'Parent Exhibit:', 'gallery' ),
+    'menu_name' => _x( 'Exhibits', 'gallery' ),
+    );
+    $args = array(
+    'labels' => $labels,
+    'hierarchical' => false,
+    'description' => 'Exhibits here will be displayed in the Gallery. ',
+    'supports' => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'post-formats' ),
+    'taxonomies' => array( 'category', 'post_tag' ),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_nav_menus' => true,
+    'publicly_queryable' => true,
+    'exclude_from_search' => false,
+    'has_archive' => true,
+    'query_var' => true,
+    'can_export' => true,
+    'rewrite' => true,
+    'capability_type' => 'post'
+    );
+    register_post_type( 'gallery', $args );
+    } 
+    
+
+
