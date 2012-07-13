f7c97fe11a785e96083495c24d4eb4488dadf27d
JohnIsCoding <info@pigeondesign.net>
2012-07-13 14:38:40 +0100

    made thumbnails link to full size

--- loop-gallery.php
+++ loop-gallery.php
@@ -8,8 +8,16 @@
     <?php roots_post_inside_before(); ?>
       <?php // the_content(); ?>
      
+<?php 
+ if ( has_post_thumbnail()) {
+   $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large');
+   echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" >';
+   echo get_the_post_thumbnail($post->ID, 'thumbnail-small'); 
+   echo '</a>';
+ }
+?>
  
-       <a href="<?php the_permalink(); ?>"><?php echo get_the_post_thumbnail($post->ID, 'thumbnail-small'); ?></a>
+
 
      
       <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
