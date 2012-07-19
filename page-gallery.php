1f8b9a35eb413f99c375366d189a6941f3433c71
JohnIsCoding <info@pigeondesign.net>
2012-07-19 11:22:54 +0100

    tinkering with gallery

--- page-gallery.php
+++ page-gallery.php
@@ -17,41 +17,50 @@ get_header(); ?>
 <?php 		$images = get_field('gallery_entries');  if( $images ): ?>
    
 
-        <div id="slides">
-            
-            
-            <div class="slides_container span6">
-               
 
+
+
+
+        <div id="slides" class="row">
+            
+                       
+            <div class="slides_container span12">
+               
             <?php foreach( $images as $image ): ?>
              <div class="slide">
+                  
+          <div class="span6">
                     <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>" />
-                    <div class="caption"><?php echo $image['title']; ?></div>
-                </div>
+                    </div>
+                                      <div class="caption span5 offset1"><em><?php echo $image['title']; ?></em>
+                                      <?php //replace the serif x with the nicer-looking &times; ?>
+                                      <?php $replace = str_replace("x", "&times;", $image['description']); ?>
+                                      <p><?php echo $replace ?></p></div>
+
+
+
+                </div> <!-- .slide -->
             <?php endforeach; ?>
+            
+            
   
             
             </div> <!-- .slides_container -->
        
-        <div class="span5 offset1">
-        <ul class="slides_pagination">
+        <div id="pagination_container" class="span5 offset7">
 
+        <ul class="slides_pagination">
+                                              
     <?php //The following code creates the thumbnail navigation ?>
     
-  
             <?php foreach( $images as $image ): ?>
                 <li>
                     <a href="#"><img src="<?php echo $image['sizes']['thumbnail-small']; ?>" alt="<?php echo $image['title']; ?>" /></a>
                 </li>
             <?php endforeach; ?>
-
-
-
-
-
-
         </ul>
-       </div> <!-- .pagination -->
+
+       </div> <!-- .span5 offset1 -->
        
        
         </div> <!-- #slides -->
